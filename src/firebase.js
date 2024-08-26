7216748bb7913a34d2a857546be5bd87e3c6a7ed
hasib <mdhsbaskari@gmail.com>
2024-08-27 00:10:54 +0600

    chore: Update .gitignore and firebase.js
    Ignore the config.js file in .gitignore and update firebase.js to use the environment configuration from the config.js file.

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import envConfig from "../config";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 
-const firebaseConfig = {
-    // from .env files
-};
+const firebaseConfig = envConfig;
 
 console.log(firebaseConfig);
 
